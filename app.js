127e8b74973bb98c0ada1a36fe3dbe8ec51e8c21
Jason Z <jason.zissman@gmail.com>
2019-12-01 15:54:39 -0600

    deleting tasks once processed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
+const MongoObjectId = require('mongodb').ObjectId;
 const logger = require('./helpers/log-helper');
+const child_process = require('child_process');
 logger.setLogFilenameAndPath(process.argv[4].split("--logFile=")[1])
 
 // TODO - database manipulation cannot be done in local application. 
@@ -13,6 +15,18 @@ async function connectToDatabase(dbConnString) {
       cachedDbClient = db;
       return cachedDbClient;
     });
+}
+
+function executeTask(task) {
+  if (task && task.length > 0 && task.contains("crown") && task.contains("netflix")) {
+    child_process.exec(process.argv[6].split("--crown=")[1], (error, stdout, stderr) => {
+      logger.log(stdout);
+    });
+  } else if (task && task.length > 0 && task.contains("hulu") && task.contains("burgers")) {
+    child_process.exec(process.argv[5].split("--bobs-burgers=")[1], (error, stdout, stderr) => {
+      logger.log(stdout);
+    });
+  }
 }
 
 let checkInterval = 2000; // ms
@@ -29,7 +43,6 @@ async function main() {
     let tasks = await dbClient.db("smartfeed").collection("smartfeedItems").find({
       userId: userId
     }).toArray();
-    dbClient.close();
 
     if (tasks && tasks.length > 0) {
       logger.log(`Found ${tasks.length} relevant tasks. Processing...`);
@@ -37,12 +50,21 @@ async function main() {
       let allTasksIds = [];
       for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
-        allTasksIds.push(task._id);
-        logger.log(`must execute task ${task.thingToOpen}.`);
+        allTasksIds.push(MongoObjectId(task._id));
+        logger.log(`Executing task ${task.thingToOpen}.`);
+        executeTask(task.thingToOpen);
       }
 
-      logger.log(`must delete id ${allTasksIds}.`);
+      logger.log(`Deleting tasks with ids in ${allTasksIds}.`);
+      await dbClient.db("smartfeed").collection("smartfeedItems").deleteMany({
+        _id: {
+          $in: allTasksIds
+        }
+      });
+      logger.log(`Successfully deleted tasks with ids ${allTasksIds}.`);
     }
+
+    dbClient.close();
 
   }, checkInterval);
 }
